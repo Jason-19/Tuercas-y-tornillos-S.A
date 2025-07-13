32f1cad920fbdc30eee6e0a0b061380d4e7e14f8
Jason <nunez19jason@gmail.com>
2025-07-12 19:46:59 -0500

    integracion con procesos claves v3

--- frontend/Ferreteria/src/app/modules/products/pages/products-details/products-details.page.ts
+++ frontend/Ferreteria/src/app/modules/products/pages/products-details/products-details.page.ts
@@ -1,34 +1,108 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton,IonBreadcrumb, IonBreadcrumbs,IonIcon} from '@ionic/angular/standalone';
+import { FormsModule, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonBreadcrumb, IonBreadcrumbs, IonIcon } from '@ionic/angular/standalone';
 import { ProductsService } from '../../services/products.service';
 import { ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'app-products-details',
   templateUrl: './products-details.page.html',
   styleUrls: [],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonBreadcrumb, IonBreadcrumbs,IonIcon]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    IonButton,
+    IonBreadcrumb,
+    IonBreadcrumbs,
+    IonIcon,
+    ReactiveFormsModule
+  ]
 })
 export class ProductsDetailsPage implements OnInit {
   product: any = {};
+  newProduct: FormGroup;
+  enabled: boolean = true;
+  success: boolean = false;
+  message: string = '';
+  id: number = 0;
+
   constructor(
     private productsService: ProductsService,
-    private route: ActivatedRoute
-
-  ) { }
+    private route: ActivatedRoute,
+    private fb: FormBuilder
+  ) {
+    this.newProduct = this.fb.group({
+      id: [null, [Validators.required]],
+      name: ['', [Validators.required]],
+      descripcion: ['', [Validators.required]],
+      price: ['', [Validators.required]],
+      stock: ['', [Validators.required]],
+      idProveedor: ['', [Validators.required]],
+      idCategoria: ['', [Validators.required]],
+      idSubcategoria: ['', [Validators.required]],
+      idMarca: ['', [Validators.required]],
+      idUnidad: ['', [Validators.required]],
+      imageUrl: ['', [Validators.required]],
+    });
+  }
 
   ngOnInit() {
-   this.get_detail();
-  }
-  get_detail() {
     this.route.params.subscribe(params => {
+      this.id = +params['id']; // Convertir a número
+      this.productsService.detailProduct({ id: this.id }).subscribe({
+        next: (data: any) => {
+          this.product = data;
 
-      this.productsService.detailProduct({"id":params['id']}).subscribe(data => {
-        this.product = data
+          this.newProduct.patchValue({
+            id: data.id,
+            name: data.name,
+            descripcion: data.descripcion,
+            price: data.price,
+            stock: data.stock,
+            idProveedor: data.idProveedor,
+            idCategoria: data.idCategoria,
+            idSubcategoria: data.idSubcategoria,
+            idMarca: data.idMarca,
+            idUnidad: data.idUnidad,
+            imageUrl: data.imageUrl,
+          });
+        },
+        error: (error) => {
+          console.log(error);
+          this.message = "Error al obtener el producto";
+        }
       });
-    })
+    });
   }
 
+  editProduct() {
+    this.enabled = false;
+  }
+
+  updateProduct() {
+    console.log(this.newProduct.value); // Verifica que se esté enviando el ID correctamente
+
+    this.productsService.update_product(this.newProduct.value).subscribe({
+      next: (data) => {
+        this.message = 'Producto actualizado exitosamente';
+        this.success = true;
+        this.enabled = true;
+      },
+      error: (error) => {
+        console.log(error);
+        this.message = 'Error al actualizar el producto';
+        this.success = false;
+      }
+    });
+
+    setTimeout(() => {
+      this.success = false;
+    }, 3000);
+  }
 }
--- frontend/Ferreteria/src/environments/environment.ts
+++ frontend/Ferreteria/src/environments/environment.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
   production: false,
-  url: "http://localhost:8080/"
+  api: "http://192.168.0.6:8080"
 };
 
